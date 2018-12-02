dd8a6a0cfea9c82ba791c0a30e85f927a0b68bda
Jbrown95 <browningjustin95@gmail.com>
2018-12-02 15:16:43 -0500

    learning how to render jsx into html

---
+++ app.js
@@ -0,0 +1,14 @@
+// imports should be here
+
+const myNav = (
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+);
+
+ReactDOM.render(
+    myNav,
+    document.getElementById('app')
+);
