d5a17079ee31c39c79cc7c41e91ae7914528a64e
Jbrown95 <browningjustin95@gmail.com>
2018-12-02 15:01:50 -0500

    playing around with JSX

---
+++ jsx.js
@@ -0,0 +1,12 @@
+const myDiv = (
+    <div>
+        <h1>Jsx div element</h1>
+    </div>
+)
+
+// Always make sure there is an outermost element
+
+const noDiv = (
+    <p>Oops</p>
+    <a href="#">Wont work with not outer element</a>
+)
